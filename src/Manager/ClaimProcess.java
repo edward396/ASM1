3348530f9db3c4597b6135762b26b75f75feac0f
edward396 <s3979056@rmit.edu.vn>
2024-04-02 11:24:06 +0700

    Update ClaimProcess interface

---
+++ src/Manager/ClaimProcess.java
@@ -0,0 +1,19 @@
+/**
+ * @author <Nguyen Vo Truong Toan - s3979056>
+ */
+package Manager;
+
+import Classes.Claim;
+
+import java.util.List;
+
+public interface ClaimProcess {
+    void add(Claim claim);
+    void update(Claim claim);
+    void delete(String claimId);
+    Claim getOne(String claimId);
+    List<Claim> getAll();
+
+    void saveToFile(String fileName);
+    void loadFromFile(String fileName);
+}
