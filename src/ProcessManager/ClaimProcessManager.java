d66fcc72aa646a41da6a953638d143dc5ac64325
edward396 <s3979056@rmit.edu.vn>
2024-04-06 15:18:46 +0700

    Update better exceptions

--- src/ProcessManager/ClaimProcessManager.java
+++ src/ProcessManager/ClaimProcessManager.java
@@ -10,11 +10,41 @@ import Classes.Claim;
 import java.util.List;
 
 public interface ClaimProcessManager {
-    void add(Claim claim) throws Exception;
-    void update(Claim claim) throws Exception;
-    void delete(String claimID) throws Exception;
-    Claim getOne(String claimID) throws Exception;
-    List<Claim> getAll() throws Exception;
-    void saveToFile(String claimFilePath) throws Exception;
-    void loadFromFile(String claimFilePath) throws Exception;
+    void add(Claim claim) throws DuplicateClaimException, FileOperationException;
+
+    void update(Claim claim) throws ClaimNotFoundException, FileOperationException;
+
+    void delete(String claimID) throws ClaimNotFoundException, FileOperationException;
+
+    Claim getOne(String claimID) throws ClaimNotFoundException, FileOperationException;
+
+    List<Claim> getAll() throws FileOperationException;
+
+    void saveToFile(String claimFilePath) throws FileOperationException;
+
+    void loadFromFile(String claimFilePath) throws FileOperationException, InvalidFileFormatException;
+}
+
+class DuplicateClaimException extends Exception {
+    public DuplicateClaimException(String message) {
+        super(message);
+    }
+}
+
+class ClaimNotFoundException extends Exception {
+    public ClaimNotFoundException(String message) {
+        super(message);
+    }
+}
+
+class FileOperationException extends Exception {
+    public FileOperationException(String message) {
+        super(message);
+    }
+}
+
+class InvalidFileFormatException extends Exception {
+    public InvalidFileFormatException(String message) {
+        super(message);
+    }
 }
